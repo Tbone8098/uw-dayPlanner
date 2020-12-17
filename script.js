c6d9a059f85d8a4519d145ab196b4d770039879c
Tyler <thibault.tyler@gmail.com>
2020-12-17 10:10:14 -0800

    removing console log

--- script.js
+++ script.js
@@ -71,7 +71,6 @@ $(document).ready(function () {
 
     function displayData() {
         var allData = JSON.parse(localStorage.getItem("allData"));
-        console.log(allData);
         if (allData != null) {
             dataLength = Object.keys(allData);
             for (let i = 0; i < dataLength.length; i++) {
