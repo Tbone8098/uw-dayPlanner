069a72b2c33608a2f8cda52d7f328c68a1cc2206
Tyler <thibault.tyler@gmail.com>
2020-12-17 00:23:22 -0800

    init and Final commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Day Planner
+
+You will be able to plan out your day in the times provided.
+
+## Motivation
+
+There was no real motivation for this application other than to complete it and prove to myself that I can.
+
+## Screenshots
+
+Coming soon to a README near you...
+
+## Features
+
+You can use this application to plan out things that you might do every day. Because even though the day changes the things you put on this planner does not.
---
+++ script.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    displayData();
+    var time = new Date();
+    var now = time.getHours();
+
+    console.log(window);
+
+    const day = time.toDateString();
+
+    $("#currentDay").text(day);
+
+    var allTimeSlots = $(".container").children();
+    // console.log(allTimeSlots.length);
+    for (let i = 0; i < allTimeSlots.length; i++) {
+        var currentTimeSlot = $(allTimeSlots[i]).children()[1];
+        var time = $(currentTimeSlot).attr("data-time");
+        // console.log(currentTimeSlot);
+        if (time > now) {
+            $(currentTimeSlot).addClass("future");
+        } else if (time < now) {
+            $(currentTimeSlot).addClass("past");
+        } else if (time == now) {
+            $(currentTimeSlot).addClass("present");
+        }
+    }
+
+    $(".saveBtn").click(function () {
+        descEl = $(this).prev();
+        desc = descEl.val();
+        descId = $(descEl).attr("data-time");
+
+        // get localStorage
+        var allData = JSON.parse(localStorage.getItem("allData"));
+
+        if (allData === null) {
+            allData = [];
+        }
+        // check to see if row excises
+        dataKeys = Object.keys(allData);
+        isDataThere = false;
+        dataIndex = 0;
+        for (let i = 0; i < dataKeys.length; i++) {
+            if (allData[i].row === descId) {
+                isDataThere = true;
+                dataIndex = i;
+                break;
+            }
+        }
+        if (isDataThere) {
+            allData[dataIndex] = {
+                row: descId,
+                content: desc,
+            };
+        } else {
+            // save an array of dicts to localstorage
+            allData.push({
+                row: descId,
+                content: desc,
+            });
+        }
+
+        allData = JSON.stringify(allData);
+
+        localStorage.setItem("allData", allData);
+
+        $(descEl).attr("value", desc);
+        // console.log(descId);
+    });
+
+    function displayData() {
+        var allData = JSON.parse(localStorage.getItem("allData"));
+        dataLength = Object.keys(allData);
+        for (let i = 0; i < dataLength.length; i++) {
+            let target = $(`[data-time=${allData[i].row}]`);
+            target.val(allData[i].content);
+            console.log(target);
+        }
+    }
+});
